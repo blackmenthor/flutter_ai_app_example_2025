58083cb50b1ace172486e0615eea5c10b4dfa466
Angga Arifandi <anggadwiarifandi96@gmail.com>
2025-12-04 18:09:40 +0100

    almost complete

--- lib/config/api_config.dart
+++
@@ -1,9 +0,0 @@
-class ApiConfig {
-  // Load from environment variable
-  static String get geminiApiKey => const String.fromEnvironment('GEMINI_API_KEY', defaultValue: '');
-  
-  // Google Cloud configuration for Imagen API
-  static String get googleCloudProjectId => const String.fromEnvironment('GOOGLE_CLOUD_PROJECT_ID', defaultValue: '');
-  static String get googleCloudLocation => 'us-central1'; // Or your preferred location
-  static String get googleCloudAccessToken => ''; // This should be obtained through OAuth2
-}
